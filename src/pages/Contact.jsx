b2d01f0e37c475aa866fb32cf3291703bc74eca1
Muhammad Anas <41787190+Anaskhaan@users.noreply.github.com>
2025-06-30 23:07:57 +0500

    contact page updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,9 @@
+import ContactFormSection from "../components/Home/ContactFormSection";
+
 export const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <ContactFormSection />
+    </div>
+  );
 };
